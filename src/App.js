22a9a0e146202db0d8b1c1b3cde665230534f883
Lawal Abdulmujeeb <lawalmujeeb78@gmail.com>
2022-05-03 13:02:34 -0400

    Edited parent and child component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import Parent from "./components/Parent";
 
 function App() {
   return (
-    <div>
-      <h1>This is the App component</h1>
-    </div>
+    <>
+      <Parent />
+    </>
   )
 }
 
