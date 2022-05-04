7552a04d68adfe7c5f3ef2a8cac2565f9cf6d20f
Lawal Abdulmujeeb <lawalmujeeb78@gmail.com>
2022-05-04 10:55:58 +0100

    did the joke part bro

--- src/App.js
+++ src/App.js
@@ -1,55 +1,28 @@
 import React from "react"
-import boxes from "./boxes"
-import Box from "./Box"
 
-export default function App() {
-    const [squares, setSquares] = React.useState(boxes)
+export default function Joke(props) {
+    const [isShown, setIsShown] = React.useState(false)
 
-    function toggle(id) {
-        setSquares(function (prevState) {
-            let newList = []
+    function toggle() {
+        setIsShown(function(prevValue) {
+            return !prevValue
 
-            for (let i = 0; i < prevState.length; i++) {
-                let currentItem = prevState[i]
-                if (currentItem.id === id) {
-                    newList.push({
-                        ...currentItem,
-                        on: !currentItem.on
-                    })
-                } else {
-                    newList.push(currentItem)
-                }
-            }
-            return newList
-
-        })
-        console.log(squares)
-
-
-
-        /**
-         * Challenge: use setSquares to update the
-         * correct square in the array.
-         * 
-         * Make sure not to directly modify state!
-         * 
-         * Hint: look back at the lesson on updating arrays
-         * in state if you need a reminder on how to do this
-         */
+        })   
     }
 
-    const squareElements = squares.map(square => (
-        <Box
-            key={square.id}
-            id={square.id}
-            on={square.on}
-            toggle={toggle}
-        />
-    ))
 
+    /**
+     * Challenge:
+     * - Create state `isShown` (boolean, default to `false`)
+     * - Add a button that toggles the value back and forth
+     */
     return (
-        <main>
-            {squareElements}
-        </main>
+        <div>
+            {props.setup && <h3>{props.setup}</h3>}
+           {isShown && <p>{props.punchline}</p> }
+            <button onClick={toggle}>Show PunchLine</button>
+
+            <hr />
+        </div>
     )
 }
