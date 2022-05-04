129f833ce87a818b6d95fac0a8307c7346f6c640
Lawal Abdulmujeeb <lawalmujeeb78@gmail.com>
2022-05-04 10:07:23 +0100

    changed to box scrimba app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,55 @@
-import Parent from "./components/Parent";
+import React from "react"
+import boxes from "./boxes"
+import Box from "./Box"
 
-function App() {
-  return (
-    <>
-      <Parent />
-    </>
-  )
+export default function App() {
+    const [squares, setSquares] = React.useState(boxes)
+
+    function toggle(id) {
+        setSquares(function (prevState) {
+            let newList = []
+
+            for (let i = 0; i < prevState.length; i++) {
+                let currentItem = prevState[i]
+                if (currentItem.id === id) {
+                    newList.push({
+                        ...currentItem,
+                        on: !currentItem.on
+                    })
+                } else {
+                    newList.push(currentItem)
+                }
+            }
+            return newList
+
+        })
+        console.log(squares)
+
+
+
+        /**
+         * Challenge: use setSquares to update the
+         * correct square in the array.
+         * 
+         * Make sure not to directly modify state!
+         * 
+         * Hint: look back at the lesson on updating arrays
+         * in state if you need a reminder on how to do this
+         */
+    }
+
+    const squareElements = squares.map(square => (
+        <Box
+            key={square.id}
+            id={square.id}
+            on={square.on}
+            toggle={toggle}
+        />
+    ))
+
+    return (
+        <main>
+            {squareElements}
+        </main>
+    )
 }
-
-export default App;
--- src/components/Child.js
+++
@@ -1,8 +0,0 @@
-export default function Child(props) {
-    return (
-        <div className="child">
-            <h1>This is the child component</h1>
-            <button onClick={props.handleClick} className="child-button">Click Me</button>
-        </div>
-    )
-}
